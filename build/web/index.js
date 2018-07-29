0b071802c03b43326b89fd8b3847cb586ffe6179
Ceci Goss <ceci@ODYSSEUS.local>
2018-07-28 19:38:49 -0500

    image manipulation works

--- build/web/index.js
+++ build/web/index.js
@@ -1,12 +1,27 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const express = require("express");
+const Jimp = require("jimp");
 const app = express();
 const PORT = 3000;
-app.use(express.static('public'));
-// app.get('/', (req, res) => {
-//   res.sendfile('/static/index.html');
-// });
+app.use(express.static('src/public'));
+app.get('/', (req, res) => {
+    res.status(200).send('good!');
+});
+app.get('/testing', (req, res) => {
+    Jimp.read('./src/db/37870482_600827720311698_1512731049799450624_n.jpg', (err, image) => {
+        if (err) {
+            return console.error(err);
+        }
+        image.scale(2);
+        image.getBuffer(Jimp.MIME_PNG, (err, buffer) => {
+            if (err) {
+                return console.error(err);
+            }
+            res.type('png').status(200).send(buffer);
+        });
+    });
+});
 app.listen(3000, () => {
     console.log('Listening on port ' + PORT + '...');
 });
